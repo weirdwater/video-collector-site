77a2d033e1dd20786247b2aa9dcef6152b7724ba
Arjo <arjo@weirdwater.net>
2015-07-02 16:54:07 +0200

    Add sql file and Update README with instructions

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # imp-her
 
 A school assignment. The idea is to make a website where users can search for their favorite gaming videos, create lists and share them. It's now my task to create youtube support for the website.
+
+## Installation
+
+1. Clone this repo
+2. Have a server start with the repo as root
+3. Import imp-her.sql into your database (it creates it's own database with the correct name)
+4. Profit!
---
+++ imp-her.sql
@@ -0,0 +1,109 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.7
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost:3306
+-- Generation Time: Jul 02, 2015 at 02:49 PM
+-- Server version: 5.5.41-log
+-- PHP Version: 5.6.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `imp-her`
+--
+CREATE DATABASE IF NOT EXISTS `imp-her` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `imp-her`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE IF NOT EXISTS `categories` (
+`id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories_has_videos`
+--
+
+CREATE TABLE IF NOT EXISTS `categories_has_videos` (
+  `Category_id` int(11) NOT NULL,
+  `Videos_id` int(11) NOT NULL,
+  `datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `videos`
+--
+
+CREATE TABLE IF NOT EXISTS `videos` (
+`id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `youtube_id` varchar(11) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=23 ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id_UNIQUE` (`id`), ADD UNIQUE KEY `name_UNIQUE` (`name`);
+
+--
+-- Indexes for table `categories_has_videos`
+--
+ALTER TABLE `categories_has_videos`
+ ADD PRIMARY KEY (`Category_id`,`Videos_id`), ADD KEY `fk_Categories_has_Videos_Videos1_idx` (`Videos_id`);
+
+--
+-- Indexes for table `videos`
+--
+ALTER TABLE `videos`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id_UNIQUE` (`id`), ADD UNIQUE KEY `youtube_id_UNIQUE` (`youtube_id`), ADD UNIQUE KEY `title_UNIQUE` (`title`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `videos`
+--
+ALTER TABLE `videos`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=23;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `categories_has_videos`
+--
+ALTER TABLE `categories_has_videos`
+ADD CONSTRAINT `fk_Categories_has_Videos_Categories` FOREIGN KEY (`Category_id`) REFERENCES `categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_Categories_has_Videos_Videos1` FOREIGN KEY (`Videos_id`) REFERENCES `videos` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
