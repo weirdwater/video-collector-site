a79c57d13bb25d881002344d9b848dacba7049c9
Arjo Bruijnes <arjo@weirdwater.net>
2015-07-03 16:19:29 +0200

    Add comments Finish read

--- README.md
+++ README.md
@@ -2,9 +2,31 @@
 
 A school assignment. The idea is to make a website where users can search for their favorite gaming videos, create lists and share them. It's now my task to create youtube support for the website.
 
-## Installatie
+## Installation
 1. Have a server running PHP 5.6.8 or higher.
-2. Open `api/v1/includes/settings.php`
+2. Open `api/v1/includes/settings.php`.
 3. Change the database settings so they match your situation
 4. Go to your DBMS of your database and import `imp-her.sql`, it will make it's own database.
-5. Add categories by visiting `http://localhost/api/v1?action=insert&type=category&title=<CATEGORYTITLE>`
+5. Add categories by visiting `BASE_URL/api/v1?action=insert&type=category&title=CATEGORYTITLE`.
+6. You're all set, start collecting!
+
+* * *
+
+## Installatie
+1. Zorg er voor dat je server PHP 5.6.8 of hoger heeft.
+2. Open het bestand `api/v1/includes/settings.php`.
+3. Verander de instellingen die relevant zijn voor jouw situatie.
+4. Ga naar je DBMS en importeer `imp-her.sql`, je hoeft niet eerst een database bij te maken.
+5. Voeg een nieuwe categorie toe door te gaan naar `BASE_URL/api/v1?action=insert&type=category&title=CATEGORYTITLE`.
+6. Klaar is kees, start maar met video's verzamelen.
+
+Live versie op [mijn website](www.weridwater.net/imp-her).
+
+
+## Veranderingen
+Ten eerste heb ik mijn database anders aangepakt. Ik heb in plaats van de gegeven structuur een andere genomen. Ten eerste sla ik van videos alleen de youtube_id en de titel op. Ik heb de url en afbeelding locatie niet in de database opgeslagen, omdat je die kan opstellen met het youtube id. Het is namelijk altijd `http://www.youtube.com/watch?v=` `+` `youtube_id` en voor afbeeldingen ook. Die plak ik dus samen in het php gedeelte van mijn site. De website ontvangt altijd json met volledige informatie.
+Vervolgens heb ik de categorieën ook in een aparte tabel gestopt en door middel van een many to many relationship videos gekoppeld aan categorieën.
+
+Er laden ook een stuk meer videos, zodat de gebruiker meer videos kan bekijken voordat hij/zij de query moet aanpassen.
+
+Ook heb ik er voor gekozen om een dropdown menu te maken voor op de zoek pagina. Zo worden er niet per ongeluk nieuwe categorieën aangemaakt wanneer de gebruiker het fout spelt. Nu is het alleen iets lastiger om een categorie toe te voegen.
--- js/main.js
+++ js/main.js
@@ -11,7 +11,8 @@ $('#add-video-button').on('click', addVideoToCategory);
 $('#category-list').on('click', 'li', showCategory);
 $('#switch').on('click', switchView);
 
-
+// Hides the appropriate elements when switching views
+// Latest video bar slides out.
 function switchView()
 {
     if (view == 'collection')
@@ -37,7 +38,7 @@ function switchView()
         // Hide explore view
         $('#explore-header').hide();
         $('.explore-view').hide();
-        $('.latest-videos').animate({ 'width': '0' }, 'slow');;
+        $('.latest-videos').animate({ 'width': '0' }, 'slow');
 
         // Show collection view
         $('#collection-header').show();
@@ -45,11 +46,13 @@ function switchView()
     }
 }
 
+// Switches the videogrid to show the selected category's videos
 function showCategory(e)
 {
     getCategoryVideos(e.target.dataset.category);
 }
 
+// Gets the videos of the given category
 function getCategoryVideos(categoryId)
 {
     // Update title so it displays what category is being shown
@@ -68,9 +71,10 @@ function getCategoryVideos(categoryId)
         url: 'api/v1?action=category&category=' + categoryId
     }).done(function(response) {
         updateVideoGrid(response.data, $('#favorited-videos'), null, false);
-    })
+    });
 }
 
+// Adds a video to a category in the database
 function addVideoToCategory() {
     var video = $('#enlarged-video').prop('data-youtube-id');
     var category = $('#add-video-dropdown option:selected').prop('id');
@@ -81,16 +85,10 @@ function addVideoToCategory() {
     }).done(function () {
         $('#add-video-button').prop('value', 'Added');
         updateLatestVideos();
-        for (i = 0; i < response.data.length; i++) {
-            var video = response.data[i];
-            $('<section>', {class: 'video-grid-item', 'data-youtube-id': video.youtubeId})
-                .append($('<img>', {src: video.thumbnail, alt: video.title}))
-                .append($('<h1>', {text: video.title.substring(0, 24) + '...'}))
-                .appendTo($('.latest-videos .video-grid'));
-        }
     });
 }
 
+// Updates the sidebar to show the latest 10 videos
 function updateLatestVideos() {
     $.ajax({
         datatype: 'json',
@@ -107,6 +105,7 @@ function updateLatestVideos() {
     });
 }
 
+// Gets a list with all the categories
 function getCategories() {
     $.ajax({
         datatype: 'json',
@@ -131,6 +130,8 @@ function getCategories() {
 
 }
 
+// Places a video in the theater if the source was an image or a heading
+// If it was a load more button it will load more videos.
 function videoItemHandler(e) {
     if (e.target.tagName == 'IMG' || e.target.tagName == 'H1')
         viewVideo(e.target.parentNode.getAttribute('data-youtube-id'));
@@ -138,6 +139,7 @@ function videoItemHandler(e) {
         loadMoreVideos(e);
 }
 
+// Places a video in the theater
 function viewVideo(youtubeId) {
     $.ajax({
         datatype: 'json',
@@ -155,11 +157,13 @@ function viewVideo(youtubeId) {
     });
 }
 
+// Is activated when the searchbutton has been pressed
 function searchButtonHandler() {
     var query = $('#search-text').prop('value');
     getVideosByQuery(query);
 }
 
+// Searches the query and updates the videogrid
 function getVideosByQuery(query) {
     $('#searched-query').html(query);
     query.replace(' ', '+');
@@ -172,6 +176,7 @@ function getVideosByQuery(query) {
     });
 }
 
+// Loads more videos and updates the videogrid
 function loadMoreVideos(e) {
     e.target.setAttribute('disabled', 'disabled');
 
@@ -202,10 +207,10 @@ function updateVideoGrid(videolist, resultsBox, nextPage, reset) {
     var rows = [];
     while (videolist.length) {
         var video = videolist[0];
-        if (!i && nextPage == null)
+        if (!i && nextPage === null)
             viewVideo(video.youtubeId);
 
-        if (i % 5 == 0) {
+        if (i % 5 === 0) {
             rows[rows.length] = $('<div>', {class: 'video-grid-row'});
         }
 
