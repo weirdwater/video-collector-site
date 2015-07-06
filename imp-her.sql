5607a60738de2330aa8cda82e9f151de01c6c964
Arjo Bruijnes <arjo@weirdwater.net>
2015-07-06 12:09:22 +0200

    Repair sql file to import successfully

--- imp-her.sql
+++ imp-her.sql
@@ -1,26 +1,18 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.7
+-- version 4.2.5
 -- http://www.phpmyadmin.net
 --
--- Host: localhost:3306
--- Generation Time: Jul 02, 2015 at 02:49 PM
--- Server version: 5.5.41-log
--- PHP Version: 5.6.8
+-- Host: localhost:8889
+-- Generation Time: Jul 06, 2015 at 11:03 AM
+-- Server version: 5.5.38
+-- PHP Version: 5.5.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
 --
 -- Database: `imp-her`
 --
-CREATE DATABASE IF NOT EXISTS `imp-her` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `imp-her`;
 
 -- --------------------------------------------------------
 
@@ -28,10 +20,10 @@ USE `imp-her`;
 -- Table structure for table `categories`
 --
 
-CREATE TABLE IF NOT EXISTS `categories` (
+CREATE TABLE `categories` (
 `id` int(11) NOT NULL,
   `name` varchar(45) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 -- --------------------------------------------------------
 
@@ -39,10 +31,10 @@ CREATE TABLE IF NOT EXISTS `categories` (
 -- Table structure for table `categories_has_videos`
 --
 
-CREATE TABLE IF NOT EXISTS `categories_has_videos` (
-  `Category_id` int(11) NOT NULL,
+CREATE TABLE `categories_has_videos` (
+  `datetime` datetime NOT NULL,
   `video_id` int(11) NOT NULL,
-  `datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `category_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -51,11 +43,11 @@ CREATE TABLE IF NOT EXISTS `categories_has_videos` (
 -- Table structure for table `videos`
 --
 
-CREATE TABLE IF NOT EXISTS `videos` (
+CREATE TABLE `videos` (
 `id` int(11) NOT NULL,
   `title` varchar(100) NOT NULL,
   `youtube_id` varchar(11) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=23 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
 
 --
 -- Indexes for dumped tables
@@ -65,19 +57,19 @@ CREATE TABLE IF NOT EXISTS `videos` (
 -- Indexes for table `categories`
 --
 ALTER TABLE `categories`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id_UNIQUE` (`id`), ADD UNIQUE KEY `name_UNIQUE` (`name`);
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id_UNIQUE` (`id`);
 
 --
 -- Indexes for table `categories_has_videos`
 --
 ALTER TABLE `categories_has_videos`
- ADD PRIMARY KEY (`Category_id`,`Videos_id`), ADD KEY `fk_Categories_has_Videos_Videos1_idx` (`Videos_id`);
+ ADD PRIMARY KEY (`datetime`,`video_id`,`category_id`), ADD KEY `fk_categories_has_videos_videos_idx` (`video_id`), ADD KEY `fk_categories_has_videos_categories1_idx` (`category_id`);
 
 --
 -- Indexes for table `videos`
 --
 ALTER TABLE `videos`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id_UNIQUE` (`id`), ADD UNIQUE KEY `youtube_id_UNIQUE` (`youtube_id`), ADD UNIQUE KEY `title_UNIQUE` (`title`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -87,12 +79,12 @@ ALTER TABLE `videos`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `videos`
 --
 ALTER TABLE `videos`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=23;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- Constraints for dumped tables
 --
@@ -101,9 +93,5 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=23;
 -- Constraints for table `categories_has_videos`
 --
 ALTER TABLE `categories_has_videos`
-ADD CONSTRAINT `fk_Categories_has_Videos_Categories` FOREIGN KEY (`Category_id`) REFERENCES `categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-ADD CONSTRAINT `fk_Categories_has_Videos_Videos1` FOREIGN KEY (`video_id`) REFERENCES `videos` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ADD CONSTRAINT `fk_categories_has_videos_videos` FOREIGN KEY (`video_id`) REFERENCES `videos` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_categories_has_videos_categories1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
